b3cde0da4fe6ea559d3182ebbb76d997f09913bc
Igor Kmets <homei@mail.ru>
2021-05-22 17:46:18 +0300

    russian locale added

---
+++ src/locale/lang/ru-RU.js
@@ -0,0 +1,27 @@
+export default {
+  at: {
+    select: {
+      placeholder: 'Выбор',
+      notFoundText: 'Данные отсутствуют'
+    },
+    modal: {
+      okText: 'Ок',
+      cancelText: 'Отмена'
+    },
+    pagination: {
+      prevText: 'Назад',
+      nextText: 'Вперёд',
+      total: 'Всего',
+      item: '',
+      items: '',
+      pageSize: '/ стр.',
+      goto: 'Переход',
+      pageText: '',
+      prev5Text: 'Предыдущие 5',
+      next5Text: 'Следующие 5'
+    },
+    table: {
+      emptyText: 'Нет данных'
+    }
+  }
+}
